dade2f761430b8e4ecc23fec35016c42dba8074f
shu7bh <shubh.agarwal@live.com>
2020-08-11 22:59:12 +0300

    Added stuff in player class

--- Engine/Player.cpp
+++ Engine/Player.cpp
@@ -1,7 +1,27 @@
 #include "Player.h"
+#include "MainWindow.h"
 
 Player::Player(float t, float l, float r, float b, int rd, int grn, int bl)
 	: rect(t, l, r, b, rd, grn, bl), v(0) {}
 
 Player::Player(float t, float l, float r, float b, Color c)
 	: rect(t, l, r, b, c), v(0) {}
+
+void Player::update()
+{
+	rect.update(v, 0);
+}
+
+void Player::ChangeVelocity(MainWindow& wnd)
+{
+	if (wnd.kbd.KeyIsPressed(VK_LEFT) || wnd.kbd.KeyIsPressed('A'))
+		v = -1;
+	if (wnd.kbd.KeyIsPressed(VK_RIGHT) || wnd.kbd.KeyIsPressed('D'))
+		v = 3;
+}
+
+void Player::KeepInFrame(int Left, int Right)
+{
+	if (int(rect.left) <= Left - v || int(rect.right) >= Right - v)
+		v = 0;
+}
--- Engine/Player.h
+++ Engine/Player.h
@@ -1,13 +1,16 @@
 #pragma once
 #include "Rect.h"
+#include "MainWindow.h"
 
 class Player
 {
 public:
 	Player(float t, float l, float r, float b, int rd, int grn, int bl);
 	Player(float t, float l, float r, float b, Color c = Colors::White);
-
+	void update();
+	void ChangeVelocity(MainWindow& wnd);
 	const Rect& getRect() const { return rect; }
+	void KeepInFrame(int Left, int Right);
 private:
 	Rect rect;
 	float v;
