e7862c66754a9f5b41ef21ab9c0647aa629a2e98
shu7bh <shubh.agarwal@live.com>
2020-08-11 23:14:22 +0300

    Committing player class changes from master

--- Engine/Player.cpp
+++ Engine/Player.cpp
@@ -1,7 +1,27 @@
 #include "Player.h"
+#include "MainWindow.h"
 
-Player::Player(float t, float l, float b, float r, int rd, int grn, int bl)
-	: rect(t, l, b, r, rd, grn, bl), v(0) {}
+Player::Player(float t, float l, float r, float b, int rd, int grn, int bl)
+	: rect(t, l, r, b, rd, grn, bl), v(0) {}
 
-Player::Player(float t, float l, float b, float r, Color c)
-	: rect(t, l, b, r, c), v(0) {}
+Player::Player(float t, float l, float r, float b, Color c)
+	: rect(t, l, r, b, c), v(0) {}
+
+void Player::update()
+{
+	rect.update(v, 0);
+}
+
+void Player::ChangeVelocity(MainWindow& wnd)
+{
+	if (wnd.kbd.KeyIsPressed(VK_LEFT) || wnd.kbd.KeyIsPressed('A'))
+		v = -5;
+	if (wnd.kbd.KeyIsPressed(VK_RIGHT) || wnd.kbd.KeyIsPressed('D'))
+		v = 5;
+}
+
+void Player::KeepInFrame(int Left, int Right)
+{
+	if (int(rect.left) <= Left - v || int(rect.right) >= Right - v)
+		v = 0;
+}
